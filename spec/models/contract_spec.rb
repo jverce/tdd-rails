2f38b6e4961a617ab93d97a16e5ef7a7eb1e53fb
Jay Vercellone <juanjov@gmail.com>
2023-12-03 11:11:22 -0800

    Mark sections in new tests

--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -3,12 +3,18 @@ require "rails_helper"
 RSpec.describe Contract, type: :model do
   describe "creation" do
     it "can be created with today as the signature date" do
+      # Arrange/Act
       contract = Contract.create!
+
+      # Assert
       expect(contract.signature_date).to eq(Date.today)
     end
 
     it "can be created with a custom signature date" do
+      # Arrange/Act
       contract = Contract.create!(signature_date: Date.yesterday)
+
+      # Assert
       expect(contract.signature_date).to eq(Date.yesterday)
     end
   end
